fb0af60bda4f67afba229d4f03d2d342468d6c83
Muhammad Abdur Rafey <m.a.rafey1215@gmail.com>
2025-02-06 11:10:06 +0500

    Something done to make it work

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  // ge
   await GetStorage.init();
   runApp(const VoiceGrocery());
 }
